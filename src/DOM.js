117ec8a93181a62883233156504c5998b584dda9
ghostmatem <bebe558812@gmail.com>
2022-05-22 00:51:07 +0300

    Task has been solved

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,12 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    for (let i = 0; i < count; i++) {
+        document.body.insertAdjacentHTML(
+            'afterbegin',
+            `<${tag}>${content}</${tag}>`,
+        );
+    }
 }
 
 /*
@@ -15,6 +21,32 @@ export function appendToBody(tag, content, count) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function generateTree(childrenCount, level) {
+    document.body.insertAdjacentHTML('beforeend', '<div class="item_1"></div>');
+    insertElements(childrenCount, 1, level);
+    return document.getElementsByClassName('item_1')[0];
+}
+
+/**
+ * Осуществляет рекурсивную послойную вставку элементов
+ * @param {Number} childrenCount число элементов на каждом узле
+ * @param {Number} currentLevel текущих уровень корневых элементов
+ * @param {Number} maxLevel максимальный уровень вложености дерева
+ */
+function insertElements(childrenCount, currentLevel, maxLevel) {
+    const nextLevel = currentLevel + 1;
+
+    if (nextLevel > maxLevel) return;
+
+    const elements = document.getElementsByClassName(`item_${currentLevel}`);
+    for (const element of elements) {
+        for (let i = 0; i < childrenCount; i++) {
+            element.insertAdjacentHTML(
+                'beforeend',
+                `<div class='item_${nextLevel}'></div>`,
+            );
+        }
+    }
+    insertElements(childrenCount, nextLevel, maxLevel);
 }
 
 /*
@@ -26,4 +58,20 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    const tree = generateTree(2, 3);
+    const firstChilds = tree.getElementsByClassName('item_2');
+
+    for (const child of firstChilds) {
+        const newChild = document.createElement('section');
+        newChild.innerHTML = child.innerHTML;
+
+        for (const attribute of child.attributes) {
+            newChild.setAttribute(attribute.name, attribute.value);
+        }
+
+        child.before(newChild);
+        child.remove();
+    }
+
+    return tree;
 }
